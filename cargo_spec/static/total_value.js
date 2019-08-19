3f4a3e10c8ca7263354760910f99e343246beb3e
AndrewPie <andrzej_pietrzyk@o2.pl>
2019-08-20 01:49:14 +0200

    komentarze

--- cargo_spec/static/total_value.js
+++ cargo_spec/static/total_value.js
@@ -9,5 +9,5 @@ var total_value = function() {
 $(document).on('change', '.value', total_value);
 $(document).on('click', '#save_spec', total_value);
 
-// TODO: nie działa wywołanie funkcji w przypadku kliknięcia 'usuń'
+// FIXME: nie działa wywołanie funkcji w przypadku kliknięcia 'usuń'
 $(document).on('click', 'a.delete-row', total_value);
