58a106a19e1f445db886357da2b80c10da1aaf4f
AndrewPie <andrzej_pietrzyk@o2.pl>
2019-08-24 01:11:21 +0200

    added lp. counter to table

--- cargo_spec/tables.py
+++ cargo_spec/tables.py
@@ -1,10 +1,19 @@
+import itertools
+
 import django_tables2 as tables
 
 from cargo_spec.models import CargoContent
 
 
 class CargoContentTable(tables.Table):
-    ordinal_number = tables.Column(verbose_name='lp.')
+    ordinal_number = tables.Column(empty_values=(), verbose_name='lp.')
+    
+    def __init__(self, *args, **kwargs):
+        super(CargoContentTable, self).__init__(*args, **kwargs)
+        self.counter = itertools.count(1)
+        
+    def render_ordinal_number(self):
+        return f'{next(self.counter)}'
     
     class Meta:
         model = CargoContent
